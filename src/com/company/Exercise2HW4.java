c7ef0986a34ca47aa2305e2313d4a5b6cb4e74dd
melnykd <d.melnykk@gmail.com>
2022-01-12 16:18:44 +0200

    HW1, 6 exercises

---
+++ src/com/company/Exercise2HW4.java
@@ -0,0 +1,28 @@
+package com.company;
+import java.util.Scanner;
+
+public class Exercise2HW4
+{
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter day time");
+        int hour = scanner.nextInt();
+        if (hour > 18){
+            System.out.println("Good evening");
+        }
+        else if (hour >10) {
+            System.out.println("Good morning");
+
+        }
+        else{
+            System.out.println("Good day");
+        }
+
+
+    }
+}
+
+
+
---
+++ src/com/company/Exercise6HW4.java
@@ -0,0 +1,67 @@
+
+
+package com.company;
+import java.util.Scanner;
+class Exercise6HW4 {
+    public static void main(String[] args)
+    {
+        int month;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the month number (1-12): ");
+        month= input.nextInt();
+
+        if (month == 1)
+        {
+            System.out.println("31 days");
+        }
+        else if(month == 2)
+        {
+            System.out.println("28 or 29 days");
+        }
+        else if(month == 3)
+        {
+            System.out.println("31 days");
+        }
+        else if(month == 4)
+        {
+            System.out.println("30 days");
+        }
+        else if(month == 5)
+        {
+            System.out.println("31 days");
+        }
+        else if(month == 6)
+        {
+            System.out.println("30 days");
+        }
+        else if(month == 7)
+        {
+            System.out.println("31 days");
+        }
+        else if(month == 8)
+        {
+            System.out.println("31 days");
+        }
+        else if(month == 9)
+        {
+            System.out.println("30 days");
+        }
+        else if(month == 10)
+        {
+            System.out.println("31 days");
+        }
+        else if(month == 11)
+        {
+            System.out.println("30 days");
+        }
+        else if(month == 12)
+        {
+            System.out.println("31 days");
+        }
+        else
+        {
+            System.out.println("Invalid input! Please enter month number between (1-12).");
+        }
+
+    }
+}
